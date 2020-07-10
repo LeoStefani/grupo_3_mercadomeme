207b3847b3be9241e9c6036e1e6fef309e5e5e71
unknown <leonelstefani@gmail.com>
2020-07-10 17:25:04 -0300

    Cambie algunas rutas para que queden en app.js, actualice un poco la vista de carga de producto desntro de produtcs/upload, cree las rutas para el register dentro de users, aunque estan recien empezadas. Borre del head el title, y puse algunas reglas de body para el style general, porque sino se aplican las de los imports

--- myapp/src/routes/usersRouter.js
+++ myapp/src/routes/usersRouter.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const usersController = require('../controllers/usersController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', usersController.usersIndex);
+router.get('/register', usersController.register);
+
 
 module.exports = router;
