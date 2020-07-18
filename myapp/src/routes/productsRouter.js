be1a930a3b32b7c95a81f2c39c2d81f1f8fa03aa
aguvenditti <aguvenditti@gmail.com>
2020-07-18 17:00:18 -0300

    agrego products.json con 16 productos, aun una unica imagen

--- myapp/src/routes/productsRouter.js
+++ myapp/src/routes/productsRouter.js
@@ -2,9 +2,23 @@ var express = require('express');
 var router = express.Router();
 const productsController = require('../controllers/productsController');
 
+// General products render 
 router.get('/', productsController.productsIndex);
-router.get('/detail', productsController.detail);
+
+// Product CRUD routes
 router.get('/upload', productsController.upload);
+
+router.get('/create', productsController.upload);
+router.post('/create', productsController.upload);
+
+router.get('/:id/edit', productsController.upload);
+router.put('/:id', productsController.upload);
+router.delete('/:id', productsController.upload);
+
+// Product details 
+router.get('/detail', productsController.detail);
+router.get('/:id', productsController.productsIndex);
+
 
 
 module.exports = router;
