be4425f480efc9f7ef47b43394b0e833d6fe6c6e
aguvenditti <aguvenditti@gmail.com>
2020-07-19 11:13:53 -0300

    Agrego funcionalidad a index para que muestre los 4 mas vendidos del JSON. Ademas linkeo a detalle de producto llevando el id

--- myapp/src/controllers/indexController.js
+++ myapp/src/controllers/indexController.js
@@ -3,31 +3,32 @@ const path = require('path');
 
 let products = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf-8'));
 
-function GetSortOrder(prop) {    
-        return function(a, b) {    
-            if (a[prop] > b[prop]) {    
-                return 1;    
-            } else if (a[prop] < b[prop]) {    
-                return -1;    
-            }    
-            return 0;    
-        }    
-    };  
 
+// funcion para ordenar los productos segun cantidad vendida 
+function GetSortOrder(prop) {
+        return function (a, b) {
+                if (a[prop] > b[prop]) {
+                        return 1;
+                } else if (a[prop] < b[prop]) {
+                        return -1;
+                }
+                return 0;
+        }
+};
+// orden de los productos en sortedProducts (ATENCION! Estan de menor a mayor!)
 let sortedProducts = products.sort(GetSortOrder("qtySold"));
-let fourBestSellers;
-
-
-// console.log(fourBestSellers);
 
 
 let controller = {
-    index: function(req, res, next) {
-        res.render('index', {title: "Bienvenidos!"});
-      },
-    error: function(req, res, next) {
-        res.render('error', {title: "Error"});
-      }
+        index: function (req, res, next) {
+                res.render('index', {
+                        title: 'Bienvenidos!',
+                        sortedProducts: sortedProducts,
+                });
+        },
+        error: function (req, res, next) {
+                res.render('error', { title: "Error" });
+        }
 };
 
 module.exports = controller;
