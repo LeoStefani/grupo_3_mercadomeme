7d88c6b8ccb452289c5558aafbb45ab526f72496
Coco <jmbeytrison@gmail.com>
2020-07-22 23:19:57 -0300

    vista login terminada

--- myapp/src/controllers/usersController.js
+++ myapp/src/controllers/usersController.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
 let products = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf-8'));
+
 
 module.exports = {
     usersIndex: function (req,res,next) {
