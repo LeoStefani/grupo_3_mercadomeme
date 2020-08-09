4e7e093fd97e2d6e0f3ded059adc991002763936
aguvenditti <aguvenditti@gmail.com>
2020-08-09 15:15:56 -0300

    creo modelo Product

---
+++ myapp/src/database/models/Product.js
@@ -0,0 +1,77 @@
+module.exports = function(sequelize, dataTypes) {
+
+        let alias = "Product";
+    
+        let cols = {
+            id:{
+                type: dataTypes.INTEGER, 
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: true,
+            },
+            name:{
+                type: dataTypes.STRING,
+                allowNull: false,
+                unique: {
+                    args: true,
+                    message: 'El nombre de producto ya existe',
+                //     fields: [sequelize.fn('lower', sequelize.col('name'))]
+                },
+                validate: {
+                    notNull: { msg: 'El campo "nombre" no puede quedar vacío.'},
+                    notEmpty: { msg: 'El campo "nombre" no puede quedar vacío.'}
+                } 
+            },
+            description:{
+                type: dataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: { msg: 'El campo "descripción" no puede quedar vacío.'}
+                }  
+            },
+            price:{
+                type: dataTypes.DECIMAL,
+                notNull: true,
+                unsigned: true
+            },
+            qty_sold:{
+                type: dataTypes.INTEGER,
+                unsigned: true
+            },
+            id_category:{
+                type: dataTypes.INTEGER,
+                notNull: true
+            },
+            image_0:{
+                type: dataTypes.STRING
+            },
+            image_1:{
+                type: dataTypes.STRING
+            },
+            image_2:{
+                type: dataTypes.STRING
+            },
+            image_3:{
+                type: dataTypes.STRING
+            },
+            image_4:{
+                type: dataTypes.STRING
+            }            
+        };
+    
+        let config = {
+            tableName: "products",
+            timestamps: false,
+            indexes: [
+                {
+                    unique: true,
+                    fields: ['name']
+                }
+            ]
+        }
+    
+        let Product = sequelize.define(alias, cols, config);
+    
+        return Product;
+    
+    }
