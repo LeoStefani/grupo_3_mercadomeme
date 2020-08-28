51320df59669c560546c0bae8259566e38b3e5f5
aguvenditti <aguvenditti@gmail.com>
2020-08-27 21:31:35 -0300

    listo los mas vendidos del home

--- myapp/src/controllers/productsController.js
+++ myapp/src/controllers/productsController.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const db = require("../database/models");
-const { addListener } = require('cluster');
 const { Op } = require("sequelize");
 
 
