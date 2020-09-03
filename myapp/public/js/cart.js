6d2b09eab41292fa0a2af2936c94c2053036b386
aguvenditti <aguvenditti@gmail.com>
2020-09-03 16:51:11 -0300

    pruebas carrito

---
+++ myapp/public/js/cart.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", function () {
+
+    let qs = element => {return document.querySelector(element)};
+    let qsa = element => {return document.querySelectorAll(element)};
+    
+    let shopCartIcon = qs("#shopCartIcon");
+    
+    
+    shopCartIcon.addEventListener('click', event => {
+        event.preventDefault();
+        // Hacer el fetch y pasarle todo lo de localStorage
+        // productToCart = {
+        //     id: document.documentURI.substring(document.documentURI.lastIndexOf('/') + 1),
+        //     color: qs('#selectedColor').value,
+        //     size: qs('#selectedSize').value
+        // }
+        // console.log(productToCart);
+        alert('Clickeaste el carrito');
+
+        let data = localStorage;
+
+        fetch(shopCartIcon.href, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type' : 'application/json'
+            }.then(function(response) {
+                return response.json();
+            })
+        });
+        
+        location.href = shopCartIcon.href;
+    })
+    
+    
+    });
---
+++ myapp/public/js/detail.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", function () {
+
+    let qs = element => {return document.querySelector(element)};
+    let qsa = element => {return document.querySelectorAll(element)};
+    
+    let addToCart = qs("#addToShopCart");
+    
+    function allStorage() {
+        let values = [],
+            keys = Object.keys(localStorage),
+            i = keys.length;
+        while ( i-- ) {
+            values.push( JSON.parse(localStorage.getItem(keys[i])) );
+        }
+        return values;
+    };
+
+    addToCart.addEventListener('submit', event => {
+        event.preventDefault();
+        productToCart = {
+            id: document.documentURI.substring(document.documentURI.lastIndexOf('/') + 1),
+            color: qs('#selectedColor').value,
+            size: qs('#selectedSize').value
+        }
+        // console.log(productToCart);
+        localStorage.setItem('addedProduct' + productToCart.id ,JSON.stringify(productToCart));
+        alert('Producto agregado con éxito!!');
+        let items = allStorage(localStorage);
+        // console.log(localStorage);
+        console.log(items);
+        // localStorage.clear();
+    })
+    
+    
+    });
