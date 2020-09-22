7fc8e583f6f2277993f6d5070ec00102a621d904
unknown <leonelstefani@gmail.com>
2020-09-22 15:54:01 -0300

    previo pull

--- myapp/public/js/detail.js
+++ myapp/public/js/detail.js
@@ -17,13 +17,37 @@ window.addEventListener("load", function () {
 
     addToCart.addEventListener('submit', event => {
         event.preventDefault();
-        productToCart = {
-            id: document.documentURI.substring(document.documentURI.lastIndexOf('/') + 1),
-            color: qs('#selectedColor').value,
-            size: qs('#selectedSize').value
-        }
-        localStorage.setItem('addedProduct' + Date.now() ,JSON.stringify(productToCart));
-        alert('Producto agregado con éxito!!');
+        let dateTag = {tag: Date.now()};
+
+        fetch('/memes/rename', {
+            method: 'POST',
+            body: JSON.stringify(dateTag),
+            headers: {
+                'Content-Type' : 'application/json'
+                },
+            }).then(function(response) {
+                // console.log('11');
+                console.log(response);
+                // location.href = shopCartIcon.href;
+            }).then(function(info){
+                productToCart = {
+                    id: document.documentURI.substring(document.documentURI.lastIndexOf('/') + 1),
+                    color: qs('#selectedColor').value,
+                    size: qs('#selectedSize').value,
+                    meme: "memeUser"+dateTag.tag+".png",
+                }
+                localStorage.setItem('addedProduct' + Date.now() ,JSON.stringify(productToCart));
+                alert('Producto agregado con éxito!!');
+            //     console.log('22');
+            //     console.log(info);
+            }).catch(function (error) {
+                // console.log('33');
+                console.log(error);
+            });
+        
+
+
+
         // let items = allStorage(localStorage);
        
     })
