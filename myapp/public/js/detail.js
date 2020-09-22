b4035ed6a911abda80a6de971472563f4cd6b79c
aguvenditti <aguvenditti@gmail.com>
2020-09-22 16:39:09 -0300

    carrito resuelto con icono y numero

--- myapp/public/js/detail.js
+++ myapp/public/js/detail.js
@@ -4,6 +4,7 @@ window.addEventListener("load", function () {
     let qsa = element => {return document.querySelectorAll(element)};
     
     let addToCart = qs("#addToShopCart");
+    let myButtonCart = qs("#myButtonCart");
     
     addToCart.addEventListener('submit', event => {
         event.preventDefault();
@@ -11,7 +12,7 @@ window.addEventListener("load", function () {
         productToCart = {
             id: Number(document.documentURI.substring(document.documentURI.lastIndexOf('/') + 1)),
             color: qs('#selectedColor').value,
-            size: qs('#selectedSize').value
+            size: qs('#selectedSize').value,
         };
 
         let cart = [];
@@ -22,13 +23,23 @@ window.addEventListener("load", function () {
             cartStorage = JSON.parse(cartStorage);
             cartStorage.push(productToCart);
             localStorage.setItem('cart', JSON.stringify(cartStorage));
+            myButtonCart.innerText = cartStorage.length;
+
         } else {
             cart.push(productToCart);
             localStorage.setItem('cart', JSON.stringify(cart));
-        }; 
-
-        alert('Producto agregado con éxito!!');
-       
+            myButtonCart.innerText = cart.length;
+        };
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Producto agregado con éxito!!',
+            showConfirmButton: false,
+            timer: 1500
+          }).then( results => {
+                window.location.href = "/products/index";
+          })
+          
     });
     
 });
